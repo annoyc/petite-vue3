3512396d920a90f8a1e712dacfd84fb356145b46
kevinawe <yc2hangycz@gmail.com>
2022-04-28 14:28:51 +0800

    simple reactivity completed

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,7 +1,14 @@
 import { isObject } from "@vue/shared"
 
+const enum ReactiveFlags {
+  IS_REACTIVE = '_v_isReactive',
+}
+
 const mutableHandlers: ProxyHandler<any> = {
   get(target, key, receiver) {
+    if(key===ReactiveFlags.IS_REACTIVE) {
+      return true;
+    }
     const res = Reflect.get(target, key, receiver)
     // don't return the raw reactive object as it can still be mutable
     return res
@@ -20,13 +27,24 @@ const mutableHandlers: ProxyHandler<any> = {
   }
 }
 
+const reactiveMap = new WeakMap() // WeakMao弱引用，key必须是对象，如果key未被引用可以自动被回收
 
 function createReactiveObject(target: object) {
+
+  if ((target as any)[ReactiveFlags.IS_REACTIVE]) {
+    return target
+  }
   if(!isObject(target)) {
     return target
   }
 
+  const existingProxy = reactiveMap.get(target)
+  if(existingProxy) {
+    return existingProxy
+  }
+
   const proxy = new Proxy(target, mutableHandlers)
+  reactiveMap.set(target, proxy)
   return proxy
 }
 
