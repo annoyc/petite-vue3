62f1614c9453c4c401ffc6784efc6878c20baa3e
kevinawe <yc2hangycz@gmail.com>
2022-05-06 21:45:10 +0800

    fix: update rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,15 @@
  * @Author       : zhangyc
  * @Date         : 2022-04-26 22:00:50
  * @LastEditors  : zhangyc
- * @LastEditTime : 2022-05-05 20:41:41
+ * @LastEditTime : 2022-05-06 21:43:59
  */
 
 import path from 'path'
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
+import nodeResolve from '@rollup/plugin-node-resolve'
 
-const packageFormats =  process.env.FORMATS
+const packageFormats = process.env.FORMATS
 const sourcemap = process.env.SOURCE_MAP
 const target = process.env.TARGET
 
@@ -24,38 +24,36 @@ const pkg = require(resolveFile('package.json')) // package.json
 const external = ['debug', ...Object.keys(pkg.dependencies || {})] // external dependencies
 
 const outputConfig = {
-  "esm-bundler": {
-    format: 'es',
-    file: resolveFile(`dist/${pkg.name}.esm-bundler.js`),
-    sourcemap,
-  },
-  "cjs-bundler": {
-    format: 'cjs',
-    file: resolveFile(`dist/${pkg.name}.cjs-bundler.js`),
-    sourcemap,
-  },
-  "global": {
-    file: resolveFile(`dist/${pkg.name}.global.js`),
-    format: 'iife',
-    sourcemap
-  }
+	'esm-bundler': {
+		format: 'es',
+		file: resolveFile(`dist/${pkg.name}.esm-bundler.js`),
+		sourcemap,
+	},
+	'cjs-bundler': {
+		format: 'cjs',
+		file: resolveFile(`dist/${pkg.name}.cjs-bundler.js`),
+		sourcemap,
+	},
+	global: {
+		file: resolveFile(`dist/${pkg.name}.global.js`),
+		format: 'iife',
+		sourcemap,
+	},
 }
 
-const packageConfigs = packageFormats === 'undefined' ? pkg.buildOptions.formats : packageFormats.split(',')
+const packageConfigs =
+	packageFormats === 'undefined'
+		? pkg.buildOptions.formats
+		: packageFormats.split(',')
 
-const createConfig = (output) => ({
-  input: resolveFile(`src/index.js`),
-  output: {
-    ...output,
-    name: output.format === 'iife' ? pkg.buildOptions.name : pkg.name,
-  },
-  externa: output.format === 'iife' ? [] : external,
-  plugins: [
-    json(),
-    commonjs(),
-    nodeResolve(),
-  ],
+const createConfig = output => ({
+	input: resolveFile(`src/index.js`),
+	output: {
+		...output,
+		name: output.format === 'iife' ? pkg.buildOptions.name : pkg.name,
+	},
+	externa: output.format === 'iife' ? [] : external,
+	plugins: [json(), commonjs(), nodeResolve()],
 })
 console.log(packageConfigs.map(format => createConfig(outputConfig[format])))
 export default packageConfigs.map(format => createConfig(outputConfig[format]))
-
